2654203abc569d89626a65f52225e6459d2df1db
kubrahatice <kubra_bkc@hotmail.com>
2025-06-21 02:37:32 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int sayı = 20;
+
+if (sayı == 10)
+
+{
+    Console.WriteLine("Sayı 10'a eşittir.");
+}
+
+else if (sayı > 10)
+
+{
+    Console.WriteLine("Sayı 10'dan büyüktür.");
+}
+
+if (sayı < 10)
+
+{
+    Console.WriteLine("Sayı 10'dan küçüktür.");
+}
+
+if (sayı > 10)
+
+{
+    Console.WriteLine("Sayı çifttir.");
+}
